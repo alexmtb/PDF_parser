48b65180d79cbf0c820c25f4c5529a2908ffb687
Alexandr <aleximtb@gmail.com>
2022-11-04 19:22:33 +0300

    Обновлены: текст регулярные выражения

--- parser.py
+++ parser.py
@@ -8,15 +8,23 @@ text = ''
 with fitz.open('test_task.pdf') as doc:
     for elem in doc.pages():
         text += elem.get_text()
-# убираем лишние переносы строк из текста
-text = text.replace('\n'," ")
-print(text, '\n')
 
 # чтобы в словарь вошло все вместе с заголовком (отдельная запись), дополним строку
 text = 'TITLE: ' + text
+# убираем лишние переносы строк из текста
+text = text.replace(' \n \n',"\n")
+#print(text)
+
+# модификация текста, чтобы выровнять последнюю запись
+text_wout_notes = text[:text.find('NOTES') + 6] # срез с удалением лишнего пробела
+insp_notes = ' ' + text[text.find('NOTES') + 7:] # срез последней записи в тексте
+new_text = text_wout_notes+insp_notes # конкатенация текста и последней записи
+#new_text_split = text.split('\n')
+print(new_text)
+
 
 # с помощью рег.выражений модуля 're' формируем пары для словарей из текста
-text_reg = re.findall(r'(\w+): ((?:\w+\s)+)', text)
+text_reg = re.findall(r'(.+): ((?:.+))', new_text)
 # print(text_reg, '\n')
 # формируем словарь из списка с кортежами
 text_dict = dict((key, value.strip()) for key, value in text_reg)
@@ -24,7 +32,7 @@ text_dict = dict((key, value.strip()) for key, value in text_reg)
 # проверка словарей, вывод на экран
 print('Текст из файла в виде словарей:\n')
 for key, value in text_dict.items():
-    print(f'{key}: {value}')
+    print(f'{key}: {value}', end = '\n')
 
 # запись в текстовый файл
 file = open('dict_text_out.txt', 'w')
